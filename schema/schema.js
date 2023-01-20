a7d8eb202c5831785302b4584d28c7c1bfede30a
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-20 15:32:25 +0100

    Added Book Query

--- schema/schema.js
+++ schema/schema.js
@@ -11,3 +11,18 @@ const BookType = new GraphQLObjectType({
         genre: {type: GraphQLString}
     })
 });
+
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: {id : {type:GraphQLString }},
+            resolve(parent, args){
+
+            }
+        }
+      
+    }
+})
