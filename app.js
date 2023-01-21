82f3333a4013ad0c2127564c27e679ed939b6e9e
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-21 16:19:25 +0100

    Connection Setup with MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
 
 const app = express();
+
+let URL = "mongodb+srv://Christsam:SamFajobi99@cluster0.otx0rri.mongodb.net/?retryWrites=true&w=majority"
+mongoose.set("strictQuery", false);
+mongoose.connect(URL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+    console.log("Database Connection Successful!!!");
+});
 
 
 app.use("/graphql", graphqlHTTP({
@@ -11,7 +24,7 @@ app.use("/graphql", graphqlHTTP({
     graphiql: true
 }))
 
-const PORT = 5000;
+const PORT = 5000;  
 
 app.listen(PORT, (req, res ) => { 
     console.log(`App is Listening on PORT Port ${PORT}`)
